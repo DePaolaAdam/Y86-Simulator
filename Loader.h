83cb4d0b0647bafe591b31c9316ca6879a484f45
Adam <depaolaat@appstate.edu>
2018-10-04 18:26:06 -0400

    changed header

--- Loader.h
+++ Loader.h
@@ -6,5 +6,13 @@ class Loader
       std::ifstream inf;  //input file handle
    public:
       Loader(int argc, char * argv[]);
+	void loadFile(std::string file, bool & error);
+	bool checkSpaces(std::string line, uint64_t start, uint64_t end);
+	bool loadline(std::string line, uint64_t *addr, bool & error);
+	bool checkAddress(std::string line, uint64_t *addr, bool & error);
+	uint64_t getAddress(std::string line);
+	int checkData(std::string line);
+	bool checkHex(std::string line, uint64_t start, uint64_t end);
+	void storeData(std::string line, uint64_t addr, bool & error);
       bool isLoaded();
 };
